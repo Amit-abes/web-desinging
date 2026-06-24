946e4b66dd298f00ba76ed17ad5a9e966110112d
Amit-abes <giri10112006@gmail.com>
2026-06-24 15:11:45 +0530

    Add files via upload

---
+++ lowecaseuppercase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    char ch;
+    cout<<"enter the char";
+    cin>>ch;
+if('a'<=ch && ch<= '2'){
+    cout<<"lowercase";
+}
+else{
+    cout<<"uppercase";
+}
+}
